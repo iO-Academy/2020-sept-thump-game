c18c6eaf2ce02ad0e4c85aa733e2c7fc93598161
Arth Pajor <adrasi@gmail.com>
2020-10-13 14:17:24 +0100

    starting on the next task

--- game.js
+++ game.js
@@ -2,10 +2,6 @@
 //     return Math.round(Math.random() * (max - min) + min);
 // }
 
-var trumps = document.querySelectorAll('.trump');
-var building = document.querySelectorAll('.building');
-
-
 // function visibleTrump() {
 //     trumps.forEach((trump) => {
 //         trump.hidden = true;
@@ -13,3 +9,27 @@ var building = document.querySelectorAll('.building');
 // }
 //
 // visibleTrump();
+
+
+var trumps = document.querySelectorAll('.trump');
+var building = document.querySelectorAll('.building');
+
+
+function randTime(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getPosition() {
+    var positions = ['.animateTop', '.animateLeft', '.animateRight'];
+    var length = positions.length;
+    return Math.floor(Math.random() * length);
+}
+
+
+function randTrump() {
+    var time = randTime(1000, 2000);
+    trumps.forEach((trump) => {
+         trump.classList.add('animate');
+     })
+
+}
