dcb77df8809503a90e20d6641e535ff7534c1307
Arth Pajor <adrasi@gmail.com>
2020-10-13 13:30:32 +0100

    function visibleTrumps added

---
+++ game.js
@@ -0,0 +1,15 @@
+// function randTime(min, max) {
+//     return Math.round(Math.random() * (max - min) + min);
+// }
+
+var trumps = document.querySelectorAll('.trump');
+var building = document.querySelectorAll('.building');
+
+
+// function visibleTrump() {
+//     trumps.forEach((trump) => {
+//         trump.hidden = true;
+//     })
+// }
+//
+// visibleTrump();
