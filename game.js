5f158337a6aefcb96685a3ae6e5a45811ecb2115
Richard Catterill <r_catterill@hotmail.com>
2020-10-14 14:52:49 +0100

    Merging in story 5

--- game.js
+++ game.js
@@ -33,23 +33,16 @@ function getRandom(array) {
     return array[idx];
 }
 
-/**
- * Adds and removes randomly selected classes to randomly selected trumps
- *
- */
-function randTrump() {
-    setInterval(() => {
-        var randomTrump = document.querySelector(getRandom(trumps))
-        var randPosition = getRandom(positions);
-        randomTrump.classList.add('animate', randPosition);
-        setTimeout(() => {
-                randomTrump.classList.remove('animate', randPosition);
-            },1500)
-        console.log(randomTrump)
-    }, randTime(2000, 4000));
-}
 
-randTrump();
+let gameActive = setInterval(() => {
+    let randomTrump = document.querySelector(getRandom(trumps))
+    let randPosition = getRandom(positions);
+    randomTrump.classList.add('animate', randPosition);
+    setTimeout(() => {
+            randomTrump.classList.remove('animate', randPosition);
+        },1500)
+}, randTime(2000, 4000));
+
 
 let trumpsDiv = document.querySelectorAll('.trump');
 let score = 0;
@@ -63,38 +56,44 @@ trumpsDiv.forEach(trump => trump.addEventListener('click', () => {
     }
 }))
 
-var modalContainer = document.querySelector(".modalContainer")
-var modal = document.querySelector(".modal")
-var gameOverTitle = document.querySelector(".gameOverTitle h1")
-console.log(gameOverTitle)
-var gameOverMess = document.querySelector(".gameOverMess h3")
-var gif = document.querySelector(".gif img")
-
-
-
+const modalContainer = document.querySelector(".modalContainer")
+const modal = document.querySelector(".modal")
+const gameOverTitle = document.querySelector(".gameOverTitle h1")
+const gameOverMess = document.querySelector(".gameOverMess h3")
+let gif = document.querySelector(".gif img")
 let title
 let message
-switch (true) {
-    case score === 0:
-        title = "LOOOOSER"
-        message = "ahahah I won BIGLY!!"
-        gif.src = "https://media.giphy.com/media/Qjmp5vKEERPyw/source.gif"
-        break
-    case score === 1:
-        title = "SO CLOOOOOSE"
-        message = "You got me once looooser"
-        gif.src = "https://media.giphy.com/media/wJNGA01o1Zxp6/source.gif"
-        break
-    case score < 10:
-        title = "DAAAMN"
-        message = `You thumped me ${score} times!`
-        gif.src = "https://media.giphy.com/media/6L015gMEW3pFC/source.gif"
-        break
-    default :
-        title = "FAKE NEWS!!!"
-        message = `You didn't thump me ${score} times!`
-        gif.src = "https://media.giphy.com/media/mpfMDb6MB6EWQ/giphy.gif"
-}
 
-gameOverTitle.textContent = title;
-gameOverMess.textContent = message;
+let timer = document.querySelector('.timer').textContent;
+let countdown = setInterval(() => {
+    timer--;
+    document.querySelector('.timer').textContent = timer;
+    if (timer <= 0) {
+        clearInterval(countdown);
+        clearInterval(gameActive);
+        modalContainer.style.visibility = 'visible';
+        switch (true) {
+            case score === 0:
+                title = "LOOOOSER"
+                message = "ahahah I won BIGLY!!"
+                gif.src = "https://media.giphy.com/media/Qjmp5vKEERPyw/source.gif"
+                break
+            case score === 1:
+                title = "SO CLOOOOOSE"
+                message = "You got me once looooser"
+                gif.src = "https://media.giphy.com/media/wJNGA01o1Zxp6/source.gif"
+                break
+            case score < 10:
+                title = "DAAAMN"
+                message = `You thumped me ${score} times!`
+                gif.src = "https://media.giphy.com/media/6L015gMEW3pFC/source.gif"
+                break
+            default :
+                title = "FAKE NEWS!!!"
+                message = `You didn't thump me ${score} times!`
+                gif.src = "https://media.giphy.com/media/mpfMDb6MB6EWQ/giphy.gif"
+        }
+        gameOverTitle.textContent = title;
+        gameOverMess.textContent = message;
+    }
+}, 1000);
